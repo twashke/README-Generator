653c0abb6cc278ce943341ea81c80c54fc94fd43
Tiffany Washke <twashke@gmail.com>
2021-05-17 16:02:52 -0700

    updated js file with const variables and started inquirer prompt section

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateReadMe = (answers) =>
+``;
+
+inquirer 
+    .prompt([
+        {
+            type: "",
+            name: "",
+            message: "",
+        }
+    ])
