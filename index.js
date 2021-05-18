23eb49a442a410b99d4d2129733f1b8fbc742e5a
Tiffany Washke <twashke@gmail.com>
2021-05-17 21:34:50 -0700

    added questions to index.js

--- index.js
+++ index.js
@@ -4,21 +4,58 @@ const fs = require("fs");
 const util = require("utils");
 const generateMarkdown = require("./utils/generateMarkdown.js")
 
-// TODO: Create an array of questions for user input
 // Questions for User Input
 const questions = [
     {
-        type: "",
-        name: "",
-        message: "",
-    }
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter any installation instructions.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information for the project.",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Include any contribution guidelines.",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Include test instructions.",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub name?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt = questions;
+}
 
 // Function call to initialize app
 init();
