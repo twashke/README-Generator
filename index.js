cc36fc1bf3e4c592f72ffa48271c15daa9260a26
Tiffany Washke <twashke@gmail.com>
2021-05-17 20:50:11 -0700

    updated index.js file

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// Variables for Packages
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("utils");
 
-const generateReadMe = (answers) =>
-``;
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
-inquirer 
-    .prompt([
-        {
-            type: "",
-            name: "",
-            message: "",
-        }
-    ])
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "",
+        name: "",
+        message: "",
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
