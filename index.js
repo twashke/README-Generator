d934137e4748b8b8907a249c7be415c161ec357b
Tiffany Washke <twashke@gmail.com>
2021-05-17 21:11:34 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Variables for Packages
+// Global Variables
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("utils");
-
 const generateMarkdown = require("./utils/generateMarkdown.js")
 
-
 // TODO: Create an array of questions for user input
+// Questions for User Input
 const questions = [
     {
         type: "",
